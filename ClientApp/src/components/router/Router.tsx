b58e1da66e2116a378a3ab6c7f8f6445abcf4579
Ruben Sanchez <luis.sanchezma@uanl.mx>
2019-06-23 20:58:44 -0500

    Added dashboard and basic login functionality

---
+++ ClientApp/src/components/router/Router.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import AuthorizeRoute from "../api-authorization/AuthorizeRoute";
+import { ApplicationPaths } from "../api-authorization/ApiAuthorizationConstants";
+import ApiAuthorizationRoutes from "../api-authorization/ApiAuthorizationRoutes";
+import Home from "../../areas/users/Home";
+
+const Routes = () => (
+  <Switch>
+    <AuthorizeRoute path="/home" component={Home} />
+    <Route
+      path={ApplicationPaths.ApiAuthorizationPrefix}
+      component={ApiAuthorizationRoutes}
+    />
+    <Redirect to="/home" />
+  </Switch>
+);
+
+export default Routes;
